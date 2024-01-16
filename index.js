a3e0248dee5f9e68683cf39e815d5e29410cd57d
Gachure <your.email@example.com>
2024-01-16 09:26:21 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
 }
+function hasTargetSum(arr, target) {
+  const seenNumbers = new Set();
+
+  for (const num of arr) {
+      const complement = target - num;
+
+      if (seenNumbers.has(complement)) {
+          return true;
+      }
+
+      seenNumbers.add(num);
+  }
+
+  return false;
+}
 
 /* 
   Write the Big O time complexity of your function here
